4aec78afc3c50f93df6fb2b5a0df770b7f006f2c
camilo <cam.trivino@gmail.com>
2024-05-19 21:57:49 -0400

    add

--- Lesson_03/build/js/main.js
+++ Lesson_03/build/js/main.js
@@ -6,3 +6,23 @@ stringArray[0] = '1';
 stringArray.push('four');
 guitars[0] = 1984;
 guitars.unshift();
+// stringArray = guitars ---> error: Type (string | number)[] is not assignable to type string[]
+// Type string | number is not assignable to type string
+// Type number is not assignable to type string
+guitars = stringArray;
+mixedData = guitars;
+// guitars = mixedData ---> error: Type (string | number | boolean)[] is not assignable to type (string | number)[]
+// Type string | number | boolean is not assignable to type string | number
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// tuple
+let mytuple = ['Dave', 42, true];
+let mixed = ['Van Halen', 5150, true];
+mixed = mytuple;
+// mytuple = mixed --->  Error: Type (string | number | boolean)[] is not assignable to type [string, number, boolean]
+// Target requires 3 element(s) but source may have fewer.
+// Object
+let myObject;
+myObject = [];
+console.log(typeof myObject);
--- Lesson_03/src/main.ts
+++ Lesson_03/src/main.ts
@@ -7,3 +7,32 @@ stringArray.push('four');
 
 guitars[0]= 1984
 guitars.unshift()
+
+// stringArray = guitars ---> error: Type (string | number)[] is not assignable to type string[]
+// Type string | number is not assignable to type string
+// Type number is not assignable to type string
+guitars = stringArray
+
+mixedData = guitars
+// guitars = mixedData ---> error: Type (string | number | boolean)[] is not assignable to type (string | number)[]
+// Type string | number | boolean is not assignable to type string | number
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// tuple
+let mytuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['Van Halen', 5150, true]
+
+mixed = mytuple
+// mytuple = mixed --->  Error: Type (string | number | boolean)[] is not assignable to type [string, number, boolean]
+// Target requires 3 element(s) but source may have fewer.
+
+
+// Object
+let myObject: object
+myObject = []
+console.log(typeof myObject)
+
